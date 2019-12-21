1034ed5415e556dc1bdb03e6c1a323a657caccae
jay-jain <jjain654@gmail.com>
2019-12-20 20:54:38 -0600

    Updated Lambda some more

--- buildSiteLambda.py
+++ buildSiteLambda.py
@@ -8,18 +8,28 @@ def lambda_handler(event, context):
 
 	sns = boto3.resource('sns')
 	topic = sns.Topic('arn:aws:sns:us-east-1:076858682202:DeploySciTingsTopic')
-
+	location={
+		"bucketName": 'build.sci-tings.org',
+		"objectKey": 'sci-tings-build.zip'
+	}
 	try:
+		job = event.get("CodePipeline.job")
+		if job:
+			for artifact in job["data"]["inputArtifacts"]:
+				if artifact["name"] == "MyAppBuild":
+					location = artifact["location"]["s3Location"]
+		
+		print("Building Sci - Tings from", str(location))
 		s3 = boto3.resource('s3',config = Config(signature_version = 's3v4'))
 
 		st_bucket = s3.Bucket('sci-tings.org') # Name of source code bucket
-		build_bucket = s3.Bucket('build.sci-tings.org') # Name of bucket that holds build artifact
+		build_bucket = s3.Bucket(location["bucketName"]) # Name of bucket that holds build artifact
 		
 		st_zip = io.BytesIO() # holds file object in memory temporarily
 	    
 	    # Download build artifact
 		print("Downloading build artifact.........")
-		build_bucket.download_fileobj('sci-tings-build.zip',st_zip)
+		build_bucket.download_fileobj(location["objectKey"],st_zip)
 
 		# Uploading files to source code bucket
 		with zipfile.ZipFile(st_zip) as myzip: # Provide alias name to build artifact zip file
@@ -30,6 +40,9 @@ def lambda_handler(event, context):
 				print("uploaded....",nm)
 
 		topic.publish(Subject="Sci-Tings Deployment", Message="It looks like Sci-Tings has been deployed. Please check the site to verify a successful deployment.")
+		if job:
+			codepipeline = boto3.client('codepipeline')
+			codepipeline.put_job_success_result(jobId=job["id"])
 	except:
 		topic.publish(Subject="Sci-Tings Deployment Failed", Message = "Sci-Tings failed to deploy.")
 		raise
